6fcc44ef6ab35191aee249df9554b5137ec7bb90
anoop <anoop.jasola@gmail.com>
2021-06-02 10:31:30 +0530

    progress

--- src/main/java/guru/springframework/msscbrewery/services/BeerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerService.java
@@ -1,12 +1,15 @@
 package guru.springframework.msscbrewery.services;
 
-import guru.springframework.msscbrewery.web.model.BeerDto;
-
+import java.util.List;
 import java.util.UUID;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
 
 /**
  * Created by jt on 2019-04-20.
  */
 public interface BeerService {
     BeerDto getBeerById(UUID beerId);
+    List<BeerDto> getAll();
+    BeerDto saveBeer(BeerDto beerDto);
 }
--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -1,20 +1,38 @@
 package guru.springframework.msscbrewery.services;
 
-import guru.springframework.msscbrewery.web.model.BeerDto;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
+import guru.springframework.msscbrewery.web.model.BeerDto;
 
 /**
  * Created by jt on 2019-04-20.
  */
 @Service
 public class BeerServiceImpl implements BeerService {
+	
+	private static List<BeerDto> list = new ArrayList<>();
     @Override
     public BeerDto getBeerById(UUID beerId) {
         return BeerDto.builder().id(UUID.randomUUID())
                 .beerName("Galaxy Cat")
-                .beerStyle("Pale Ale")
+                .beerStyle("Pale Ale").upc(100l)
                 .build();
     }
+
+	@Override
+	public BeerDto saveBeer(BeerDto beerDto) {
+		beerDto.setId(UUID.randomUUID());
+		list.add(beerDto);
+		return beerDto;
+	}
+
+	@Override
+	public List<BeerDto> getAll() {
+		// TODO Auto-generated method stub
+		return list;
+	}
 }
